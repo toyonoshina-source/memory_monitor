1fe5762e8fed29be9b98f0bcfd4dfd02f5e48ab2
tatsuya ishida <toyonoshina@gmail.com>
2026-08-16 11:38:03 +0800

    Initial Commit

---
+++ memory_monitor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  memory_monitor
+//
+//  Created by 石田達矢 on 2026/08/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ memory_monitor/memory_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  memory_monitorApp.swift
+//  memory_monitor
+//
+//  Created by 石田達矢 on 2026/08/16.
+//
+
+import SwiftUI
+
+@main
+struct memory_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
